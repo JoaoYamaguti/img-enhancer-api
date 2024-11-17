9f7faac043a6bfeb81534386efeb122fda479c2a
JoaoYamaguti <joaov.yamaguti@gmail.com>
2024-11-17 18:54:15 -0300

    feat: Validator for Session Controller

--- src/modules/session/session.controller.ts
+++ src/modules/session/session.controller.ts
@@ -1,17 +1,14 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import SessionService from './session.service';
 import { Response } from 'express';
+import { CreateSessionReqDto } from './session.req.dto';
 
 @Controller('session')
 class SessionController {
   public constructor(private sessionServices: SessionService) {}
   @Post()
-  public async login(
-    @Body('email') email,
-    @Body('password') password,
-    @Res() res: Response,
-  ) {
-    const service = await this.sessionServices.login(email, password);
+  public async login(@Body() body: CreateSessionReqDto, @Res() res: Response) {
+    const service = await this.sessionServices.login(body.email, body.password);
     return res.status(service.statusCode).json(service.message);
   }
 }
