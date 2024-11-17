548254d750aeb2f51d9a2716cc5e3022f80399c5
JoaoYamaguti <joaov.yamaguti@gmail.com>
2024-11-17 17:38:35 -0300

    refactor: auth middleware

--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -8,7 +8,6 @@ import { promisify } from 'util';
 class AuthMiddleware implements NestMiddleware {
   public async use(@Req() req, @Res() res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization;
-    console.log(req.headers.authorization);
 
     if (!authHeader) {
       console.log('cheguei');
@@ -21,7 +20,7 @@ class AuthMiddleware implements NestMiddleware {
       const decoded = await promisify(jwt.verify)(token, authConfig.secret);
       req.userId = decoded.id;
     } catch (error) {
-      return res.status(401).json({ error: 'Not Authorized' });
+      return res.status(401).json({ message: 'Not Authorized', error });
     }
 
     next();
