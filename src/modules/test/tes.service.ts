8992a679113f2c13df3664ba58a1ef8e66f9a00b
JoaoYamaguti <joaov.yamaguti@gmail.com>
2024-11-18 11:58:56 -0300

    delete: teste module

--- src/modules/test/tes.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-@Injectable()
-export default class TestService {
-  public sum(num1: number, num2: number): number {
-    return num1 + num2;
-  }
-}
--- src/modules/test/test.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import TestController from './test.controller';
-import TestService from './tes.service';
-
-@Module({
-  providers: [TestService],
-  controllers: [TestController],
-})
-export default class TestModule {}
