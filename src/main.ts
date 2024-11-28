2cfc62d5710e3004762b1b0fb473b259bfc4f09b
JoaoYamaguti <joaov.yamaguti@gmail.com>
2024-11-27 22:45:45 -0300

    feat: add origin to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.WEBSERVICE,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ limit: '10mb', extended: true }));
